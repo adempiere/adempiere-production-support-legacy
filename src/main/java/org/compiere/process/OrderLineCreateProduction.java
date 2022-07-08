e2ee49115fbb4623f6f031acfb6d757afc0eb781
Yamel Senih <ysenih@erpya.com>
2022-07-08 15:18:21 -0400

    Initial commit with compatibility of production

---
+++ src/main/java/org/compiere/process/OrderLineCreateProduction.java
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * The contents of this file are subject to the   Compiere License  Version 1.1
+ * ("License"); You may not use this file except in compliance with the License
+ * You may obtain a copy of the License at http://www.compiere.org/license.html
+ * Software distributed under the License is distributed on an  "AS IS"  basis,
+ * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License for
+ * the specific language governing rights and limitations under the License.
+ * The Original Code is Compiere ERP & CRM Smart Business Solution. The Initial
+ * Developer of the Original Code is Jorg Janke. Portions created by Jorg Janke
+ * are Copyright (C) 1999-2005 Jorg Janke.
+ * All parts are Copyright (C) 1999-2005 ComPiere, Inc.  All Rights Reserved.
+ * Contributor(s): ______________________________________.
+ * 
+ * Modified by Paul Bowden 
+ * ADAXA 
+ *****************************************************************************/
+package org.compiere.process;
+ 
+/**
+ *	Create (Generate) Invoice from Shipment
+ *	
+ *  @author Jorg Janke
+ *  @version $Id: OrderLineCreateProduction.java,v 1.1 2007/07/23 05:34:35 mfuggle Exp $
+ *  
+ *  @author Mario Calderon, mario.calderon@westfalia-it.com, Systemhaus Westfalia, http://www.westfalia-it.com
+ * 	@author Yamel Senih, ysenih@erpcya.com, ERPCyA http://www.erpcya.com
+ * 		<a href="https://github.com/adempiere/adempiere/issues/648">
+ * 		@see FR [ 648 ] Add Support to document Action on Standard Production window</a>
+ */
+public class OrderLineCreateProduction extends org.compiere.production.process.OrderLineCreateProduction {
+
+}	//	OrderLineCreateShipment
---
+++ src/main/java/org/compiere/process/ReplenishReportProduction.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+ * Product: Adempiere ERP & CRM Smart Business Solution                       *
+ * Copyright (C) 1999-2006 ComPiere, Inc. All Rights Reserved.                *
+ * This program is free software; you can redistribute it and/or modify it    *
+ * under the terms version 2 of the GNU General Public License as published   *
+ * by the Free Software Foundation. This program is distributed in the hope   *
+ * that it will be useful, but WITHOUT ANY WARRANTY; without even the implied *
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.           *
+ * See the GNU General Public License for more details.                       *
+ * You should have received a copy of the GNU General Public License along    *
+ * with this program; if not, write to the Free Software Foundation, Inc.,    *
+ * 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA.                     *
+ * For the text or an alternative of this public license, you may reach us    *
+ * ComPiere, Inc., 2620 Augustine Dr. #245, Santa Clara, CA 95054, USA        *
+ * or via info@compiere.org or http://www.compiere.org/license.html           *
+ * Contributor(s): Chris Farley - northernbrewer                              *
+ *****************************************************************************/
+package org.compiere.process;
+
+/**
+ *	Replenishment Report
+ *	
+ *  @author Jorg Janke
+ *  @version $Id: ReplenishReport.java,v 1.2 2006/07/30 00:51:01 jjanke Exp $
+ *  
+ *  Carlos Ruiz globalqss - integrate bug fixing from Chris Farley
+ *    [ 1619517 ] Replenish report fails when no records in m_storage
+ *  @author Mario Calderon, mario.calderon@westfalia-it.com, Systemhaus Westfalia, http://www.westfalia-it.com
+ *	@author Yamel Senih, ysenih@erpcya.com, ERPCyA http://www.erpcya.com
+ * 		<a href="https://github.com/adempiere/adempiere/issues/648">
+ * 		@see FR [ 648 ] Add Support to document Action on Standard Production window</a>
+ */
+public class ReplenishReportProduction extends org.compiere.production.process.ReplenishReportProduction {
+
+}	//	Replenish
